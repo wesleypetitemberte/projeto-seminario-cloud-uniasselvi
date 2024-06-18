b14c22a58a009b753a32214376e9cb8b8f58d974
Wesley Petitemberte W3 <wesleypetitemberte@gmail.com>
2024-06-17 22:52:03 -0300

    Fix: URL

--- pages/cadastro-usuario.php
+++ pages/cadastro-usuario.php
@@ -1,7 +1,7 @@
 <div class="itens">
   <section class="page-cadastro">
     <h1>Novo usuário</h1>
-    <form action="?page=usuario-action" method="POST" id="formCadastro">
+    <form action="./?page=usuario-action" method="POST" id="formCadastro">
       <input type="hidden" name="acao" value="cadastrar" />
 
       <label for="nome">Nome:</label>
--- php/components/header.php
+++ php/components/header.php
@@ -16,23 +16,23 @@ include("./php/config.php");
 
         <ul class="main-menu">
             <li>
-                <a class="botao-menu" href="?">Home</a>
+                <a class="botao-menu" href="./?">Home</a>
             </li>
 
             <?php if (!empty($_SESSION) && $_SESSION["tipo_usuario"] == 'ADMIN') : ?>
                 <li>
-                    <a class="botao-menu" href="?page=novo">Cadastrar Game</a>
+                    <a class="botao-menu" href="./?page=novo">Cadastrar Game</a>
                 </li>
 
             <?php elseif(!empty($_SESSION) && $_SESSION["tipo_usuario"] == 'CLIENTE') : ?>
                 <li>
-                    <a class="botao-menu" href="?page=meus-games">Meus Alugueis</a>
+                    <a class="botao-menu" href="./?page=meus-games">Meus Alugueis</a>
                 </li>
             <?php endif; ?>
 
             <?php if (empty($_SESSION)) : ?>
                 <li>
-                    <a class="botao-menu" href="?page=novo-usuario">Cadastro</a>
+                    <a class="botao-menu" href="./?page=novo-usuario">Cadastro</a>
                 </li>
                 <li>
                     <a class="botao-menu" href="./login.php">Login</a>
