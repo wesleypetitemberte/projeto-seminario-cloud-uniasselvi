3108b85e04d9c48ecee8b05f1b2e74de4e69f3b3
Wesley Petitemberte W3 <wesleypetitemberte@gmail.com>
2024-06-18 22:29:14 -0300

    Fix: Refactor list of games

--- pages/listar.php
+++ pages/listar.php
@@ -3,24 +3,21 @@
   $sql = "SELECT * FROM games WHERE qtd_disponivel > 0";
   $resposta = $conn->query($sql);
 
-  if ($resposta && $resposta->num_rows > 0) {
-    while ($linha = $resposta->fetch_object()) {
-      echo "<div>";
-      echo <<<HTML
-            <div class='card' style='--clr:#ff0;'> 
-              <img width="240px" src='$linha->imagem' alt="imagem game" />
-              <h3>$linha->nome</h3>
-              <p>Gênero: $linha->genero</p>
-              <p>Plataforma: $linha->plataforma</p>
-              <p>Preco: $linha->preco</p>
-              <p>Qtd. Disponível: $linha->qtd_disponivel</p>
-            </div>
-            HTML;
-      echo "<button class='botaoalugar' onclick=\"location.href='?page=games-action&acao=alugar&id=" . $linha->id . "'\"><strong>ALUGAR</strong></button>";
-      echo "</div>";
-    }
-  } else {
-    echo "<p>Nenhum game encontrado!</p>";
-  }
-  ?>
+  if ($resposta && $resposta->num_rows > 0) :
+    while ($linha = $resposta->fetch_object()) { 
+      $link = "location.href='?page=games-action&acao=alugar&id=" . $linha->id;
+      ?>
+      <div class='card' style='--clr:#ff0;'>
+        <img width="240px" src='<?php echo $linha->imagem; ?>' alt="<?php echo $linha->nome; ?>">
+        <h3><?php echo $linha->nome; ?></h3>
+        <p>Gênero: <?php echo $linha->genero; ?></p>
+        <p>Plataforma: <?php echo $linha->plataforma; ?></p>
+        <p>Preço: <?php echo $linha->preco; ?></p>
+        <p>Qtd. Disponível: <?php echo $linha->qtd_disponivel; ?></p>
+        <button class="botaoalugar" onclick="<?php echo $link; ?>"><strong>ALUGAR</strong></button>
+      </div>
+    <?php }; ?>
+  <?php else : ?> 
+    <p>Nenhum game encontrado!</p>
+  <?php endif; ?>
 </section>
--- pages/meus-games.php
+++ pages/meus-games.php
@@ -3,25 +3,20 @@
   $sql = "SELECT g.id, g.nome, g.preco, g.genero, g.plataforma, g.imagem FROM games as g JOIN aluguel_game as ag ON g.id = ag.game_id WHERE ag.usuario_id = {$_SESSION["id_usuario"]}";
   $resposta = $conn->query($sql);
 
-  $qtd = $resposta->num_rows;
-
-  if ($qtd > 0) {
-    while ($linha = $resposta->fetch_object()) {
-      echo "<div>";
-      echo <<<HTML
-            <div class='card' style='--clr:#0fff;'> 
-              <img width="240px" src='$linha->imagem' alt="imagem game">
-              <h3>$linha->nome</h3>
-              <p>Gênero: $linha->genero</p>
-              <p>Plataforma: $linha->plataforma</p>
-              <p>Preco: $linha->preco</p>
-            </div>
-            HTML;
-      echo "<button onclick=\"location.href='?page=games-action&acao=devolver&id=" . $linha->id . "'\">Devolver</button>";
-      echo "</div>";
-    }
-  } else {
-    echo "<p>Nenhum dado encontrado!</p>";
-  }
-  ?>
+  if ($resposta && $resposta->num_rows > 0) :
+    while ($linha = $resposta->fetch_object()) { 
+      $link = "location.href='?page=games-action&acao=devolver&id=" . $linha->id;
+      ?>
+      <div class='card' style='--clr:#0fff;'>
+        <img width="240px" src='<?php echo $linha->imagem; ?>' alt="<?php echo $linha->nome; ?>">
+        <h3><?php echo $linha->nome; ?></h3>
+        <p>Gênero: <?php echo $linha->genero; ?></p>
+        <p>Plataforma: <?php echo $linha->plataforma; ?></p>
+        <p>Preço: <?php echo $linha->preco; ?></p>
+        <button class="botaoalugar" onclick="<?php echo $link; ?>"><strong>DEVOLVER</strong></button>
+      </div>
+    <?php }; ?>
+  <?php else : ?> 
+    <p>Nenhum game encontrado!</p>
+  <?php endif; ?>
 </section>
